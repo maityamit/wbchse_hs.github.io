b7d2d0fa789a44ff60eb6418246ca90cf10e8c3f
Amit Maity <74618071+maityamit@users.noreply.github.com>
2021-06-10 23:35:13 +0530

    Add files via upload

---
+++ JS/js_one.js
@@ -0,0 +1,16 @@
+function openClass12() {
+    location.href = "Class_12.html";
+ }
+
+ function openEntranceExam() {
+    location.href = "entrance_exam.html";
+ }
+
+
+ function openAboutus() {
+    location.href = "About_us.html";
+ }
+
+
+
+
